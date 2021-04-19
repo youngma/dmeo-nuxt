dfd5e5bcfb77248154abf0023eab659d364e64fc
seo minkyu <sym221@cashwali.io>
2021-04-19 15:47:37 +0900

    Add : axios, eslint setting

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
     <h1>Hello Nuxters! 👋</h1>
+    <ul>
+      <li v-for="mountain in mountains" :key="mountain.title">
+        <NuxtLink
+          :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }"
+        >
+          {{ mountain.title }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $http }) {
+    const mountains = await $http.$get('https://api.nuxtjs.dev/mountains');
+    return { mountains };
+  },
+};
+</script>
